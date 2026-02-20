0fcb5e4c5253b233f8539d21e7aa6dd11c9ed35c
Andres Kpognon <22400181@me.uvsq.fr>
2026-02-20 11:14:49 +0100

    Ajout de L'addition

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let x = addition(5,8);
+    println!("{}",x );
+    
 }
+
+fn addition(x1:i64,x2:i64) -> i64{
+   let result = x1+x2;
+   result
+}
+
